d7b6b562f0aaaae057a9f70c755a34677869ec00
mrothenbuecher <ich@m-rothenbuecher.de>
2017-11-01 22:10:50 +0100

    added icons

---
+++ NuimoFoo/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuimoFoo
+{
+
+    class Settings
+    {
+        public bool automaticSwitchBetweenProfiles { get; set; }
+        public int rotateThreshold { get; set; }
+
+        public Settings()
+        {
+            automaticSwitchBetweenProfiles = false;
+            rotateThreshold = 10;
+    }
+
+    }
+}
